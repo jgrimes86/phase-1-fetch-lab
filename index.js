e6690dbe26e6ccd3820d4ffddad32b214d865c40
Jim Grimes <jimgrimes86@gmail.com>
2023-09-15 14:01:03 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(GoTUrl)
+  .then((bookList) => bookList.json())
+  .then(renderBooks);
 }
+
+const GoTUrl = "https://anapioficeandfire.com/api/books";
 
 function renderBooks(books) {
   const main = document.querySelector('main');
